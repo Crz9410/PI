97b99799c72dda26b3af8340c46fc97ddb4b6a76
Crz9410 <crisstofer_9410@hotmail.com>
2023-04-21 00:05:46 -0500

    se foramn las rutas de front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
-
+import Landing from './views/Landing/Landing';
+import Home from './views/Home/Home';
+import Detail from './views/Detail/Detail';
+import Form from './views/Form/Form';
+import { Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
+    <div className="App">    
+      <Route path="/home">
+        <Home />
+      </Route >
+      <Route exact path="/"   component={Landing}>
+        <Landing></Landing>
+      </Route>
+      <Route path="/form">
+        <Form />
+      </Route>
+      <Route path="/detail">
+        <Detail />
+      </Route>
+
     </div>
   );
 }
---
+++ client/src/views/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () =>{
+    return(
+        <>
+        <h1>Vista Home</h1>
+        </>
+    )
+}
+
+export default Home;
