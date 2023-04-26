dca14d85ca2241e31fc3051d1d5faa5178d62c8a
Crz9410 <crisstofer_9410@hotmail.com>
2023-04-26 09:38:34 -0500

    filtrado en orden des y ase con css en el form

--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import axios from "axios"
-
+import styles from './Form.module.css';
 const Form = () =>{
 
 const [form,setform] =useState({
@@ -42,26 +42,26 @@ console.log(err, "ño")
     return(
         <form onSubmit={submitHandler}> 
         <div>
-            <label>name</label>
+            <label>Nombre de la actividad</label>
             <input type="text" value={form.name} onChange={changeHandler} name="name" />
         </div>
         <div>
-            <label>dificultad</label>
+            <label>Dificultad</label>
             <input type="text"value={form.dificultad} onChange={changeHandler} name="dificultad" />
         </div>
         <div>
-            <label>duracion</label>
+            <label>Duracion</label>
             <input type="text" value={form.duracion} onChange={changeHandler} name="duracion" />
         </div>
         <div>
-            <label>temporada</label>
+            <label>Temporada</label>
             <input type="text" value={form.temporada} onChange={changeHandler} name="temporada" />
         </div>
         <div>
-            <label>body</label>
+            <label>Descripcion</label>
             <input type="text" value={form.body} onChange={changeHandler} name="body"/>
         </div>
-        <button type="submit">SUBMIT</button>
+        <button type="submit">Crear actividad</button>
         </form>
     )
 }
